e8e9368d186c7015480c427217b7b37e68c84028
Dennis S. Wallace <dev@shell.net>
2025-09-06 01:37:49 -0500

    Add sesion2 using expressjs

---
+++ sesion2/server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+let json ={
+    nombre: 'Adrian',
+    edad: 18,
+    esProgramador: true,
+    lenguajes: ['js','java','c#'],
+    frameworks:{
+        fronted:'Angular',
+        backend:'Express'
+    }
+}
+
+app.get('/', (req ,res)=>{
+    res.send('hola desde express');
+})
+
+app.get('/sesion2', (req, res)=>{
+    res.send('Bienvenido a la sesion2');
+})
+
+app.post('/sesion4',(req, res)=>{
+    const body = req.body;
+    console.log(body);
+    res.send('ok,recibimos tus datos')
+})
+
+app.listen(PORT, () => {
+    console.log(`Servidor escuchando en el puerto ${PORT}`);
+});
